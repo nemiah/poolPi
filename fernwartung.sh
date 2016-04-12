89326d9779e0977de0de929fc4c9bf4cd5dc46f7
Rainer Furtmeier <Rainer@Furtmeier.IT>
2016-04-12 22:47:08 +0200

    minor fix

--- fernwartung.sh
+++ fernwartung.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-ssh -o StrictHostKeyChecking=no -g -R2222:localhost:22 -R8888:localhost:80 -N nemiah@open3a.de
+ssh -o StrictHostKeyChecking=no -R\*:2222:localhost:22 -R8888:localhost:80 -N nemiah@open3a.de
