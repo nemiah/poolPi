0b009d9c1776caae5272991ca43a800b27c35667
Rainer Furtmeier <Rainer@Furtmeier.IT>
2016-04-13 15:39:49 +0200

    next step

--- startup.php
+++ startup.php
@@ -19,6 +19,10 @@ while($running){
 	
 	echo "Willkommen bei poolPi!\n";
 	echo date("d.m.Y H:i:s")."\n";
+	echo "\n";
+	
+	echo "Die IP-Adresse des Systems lautet:\n";
+	echo getHostByName(getHostName())."\n";
 	
 	sleep(1);
 }
