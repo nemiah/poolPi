a94c8f374a18f05d94a66bfd7292b5d17045f820
Rainer Furtmeier <Rainer@Furtmeier.IT>
2016-04-13 15:30:15 +0200

    startup

--- startup.php
+++ startup.php
@@ -1,8 +1,16 @@
 <?php
 
+sleep(5);
+system('clear');
+
+
 while(true){
-	echo "HI";
-	sleep(5);
+	system('clear');
+	
+	echo "Willkommen bei poolPi!\n";
+	echo date("d.m.Y H:i:s")."\n";
+	
+	sleep(1);
 }
 
 ?>
