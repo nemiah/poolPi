72ae4ac73e740c48ddd5b869d2e7c9f413fb4a7b
Rainer Furtmeier <Rainer@Furtmeier.IT>
2016-04-13 15:34:45 +0200

    hehe

--- startup.php
+++ startup.php
@@ -1,10 +1,20 @@
 <?php
+declare(ticks=1);
+
+$running = true;
+function signalHandler($signo) {
+    global $running;
+    $running = false;
+    echo "Beende...\n";
+}
+
+pcntl_signal(SIGINT, 'signalHandler');
 
 sleep(5);
 system('clear');
 
 
-while(true){
+while($running){
 	system('clear');
 	
 	echo "Willkommen bei poolPi!\n";
