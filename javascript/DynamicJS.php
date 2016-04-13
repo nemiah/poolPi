df0db1f369f4a8fcd33dc3aee8073f16fa71e9d7
Rainer Furtmeier <Rainer@Furtmeier.IT>
2016-04-13 16:38:29 +0200

    next

--- javascript/DynamicJS.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../system/connect.php";
-
-header('Content-type: text/javascript; charset="utf-8"');
-
-$DJS = new DynamicJSGUI();
-$DJS->output();
-?>
--- libraries/mailer/htmlMimeMail5.class.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once(dirname(__FILE__) . '/htmlMimeMail5.php');
-?>
