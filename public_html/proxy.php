6563ff54860be7ddd31964e21ae3041df5c6d3bb
Nena Furtmeier <Nena@Furtmeier.IT>
2024-01-24 15:29:35 +0100

    fix

--- public_html/proxy.php
+++ public_html/proxy.php
@@ -3,9 +3,24 @@
 error_reporting(E_ALL);
 ini_set('default_socket_timeout', 7);
 
+$translate = [];
+$pollFile = "/var/www/html/open3A/backend/specifics/steuerungen.poll";
+if(file_exists($pollFile)){
+	$controls = trim(file_get_contents($pollFile));
+	foreach(explode("\n", $controls) AS $line){
+		$cex = explode("\t", $line);
+		$translate[trim($cex[1])] = trim($cex[0]);
+	}
+}
+
+		
 $control = filter_input(INPUT_GET, "control");
-if($control !== null)
+if($control !== null){
+	if(strpos($control, ":") !== false AND isset($translate[trim($control)]))
+		$control = $translate[trim($control)];
+	
 	echo file_get_contents("http://$control/werte.htm");
+}
 
 $multi = filter_input(INPUT_GET, "multi");
 if($multi !== null){
@@ -14,6 +29,10 @@ if($multi !== null){
 	$return = array();
 	
 	foreach($ex AS $server){
+		$serverOrig = $server;
+		if(strpos($server, ":") !== false AND isset($translate[trim($server)]))
+			$server = $translate[trim($server)];
+		
 		$data = file_get_contents("http://$server/werte.htm");
 		if($data !== false)
 			$data = json_decode($data);
@@ -21,7 +40,7 @@ if($multi !== null){
 			$data = new stdClass();
 		
 		$s = new stdClass();
-		$s->server = $server;
+		$s->server = $serverOrig;
 		$s->data = $data;
 		
 		$return[] = $s;
