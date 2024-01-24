93b7c2b757795d49d0a045febf225bbcb7e14bb7
Nena Furtmeier <Nena@Furtmeier.IT>
2024-01-24 15:29:38 +0100

    fix

--- public_html/redeemer.php
+++ public_html/redeemer.php
@@ -3,6 +3,16 @@
 error_reporting(E_ERROR | E_PARSE);
 ini_set('default_socket_timeout', 7);
 
+$translate = [];
+$pollFile = "/var/www/html/open3A/backend/specifics/steuerungen.poll";
+if(file_exists($pollFile)){
+	$controls = trim(file_get_contents($pollFile));
+	foreach(explode("\n", $controls) AS $line){
+		$cex = explode("\t", $line);
+		$translate[trim($cex[1])] = trim($cex[0]);
+	}
+}
+
 #$m = array("type" => "success", "message" => "Befehl erfolgreich");
 #echo json_encode($m, JSON_UNESCAPED_UNICODE);
 #die();
@@ -10,6 +20,10 @@ ini_set('default_socket_timeout', 7);
 $control = filter_input(INPUT_GET, "control");
 $id = filter_input(INPUT_GET, "id");
 $value = filter_input(INPUT_GET, "value");
+
+
+if(strpos($control, ":") !== false AND isset($translate[trim($control)]))
+	$control = $translate[trim($control)];
 
 $data = file_get_contents("http://$control/modify?0003=1234");
 if($data === false){
