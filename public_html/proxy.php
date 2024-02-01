a47701ee05b279dba44e12734e859e97fd8a6e0b
Nena Furtmeier <Nena@Furtmeier.IT>
2024-02-01 20:47:04 +0100

    fix

--- public_html/proxy.php
+++ public_html/proxy.php
@@ -1,4 +1,10 @@
 <?php
+header('Expires: Thu, 01-Jan-70 00:00:01 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
 #error_reporting(E_ERROR | E_PARSE);
 error_reporting(E_ALL);
 ini_set('default_socket_timeout', 7);
